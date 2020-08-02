328b01d71d10aebd191a474b3518c088c8139a0d
jinenglengque <2441709716@qq.com>
2020-08-02 17:34:37 +0800

    首页导航

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'common/navbar/NavBar'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #ffffff
+}
 </style>
